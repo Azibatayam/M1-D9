8ee4ee5e7b90b0613331dfa129b65c89a61caa79
Azibatayam <95300506+Azibatayam@users.noreply.github.com>
2021-12-10 10:32:29 +0100

    Updated

--- index.js
+++ index.js
@@ -1,22 +1,27 @@
 
 //programmatically (using JS) a bingo board with 76 cells, numbered from 1 to 76.
 
-const createNumbers = () { 
-    let bingoCardNode = document.getElementbyId("bingoCard")
+const createNumbers = ()=> { 
+    let bingoCardNode = document.getElementById("bingoCard")
     let numbers = 76
-    let (let createNumbers = 1; ) createnumber <= numbers; dayNumbers++) {
+    for (let i = 1; i <= numbers; i++) {
         
         let eachNumber = document.createElement ("div")
-        eachNumber.innerText = numbers
+        eachNumber.innerText = i
         eachNumber.classList.add("day")
+        bingoCardNode.appendChild(eachNumber)
 
 
 }
 
+
+
 //Create a button to randomize a number from 1 to 76. The cell with same number should be highlighted in some way on the bingo board.
 
-let console = Math.ceil(Math.random() * 76)
+let random = Math.floor(Math.random() * 76)
 
 console.log(random)
 
 }
+
+createNumbers()
